fd548ea78d9acf1a023d1a0d5d7bf9d9fd26849f
elios-cama <elios.cama@gmail.com>
2022-02-26 11:21:05 +0100

    new icon state and formatting

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -15,8 +15,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   List<Article>? newsList;
   bool isLoading = true;
-
-  
+  bool _isFavorite = false;
 
   @override
   void initState() {
@@ -75,7 +74,7 @@ class _HomePageState extends State<HomePage> {
                       child: ListView.builder(
                           itemCount: newsList!.length,
                           itemBuilder: (context, index) {
-                            return ArticleTile(size, context, newsList![index]);
+                            return ArticleTiles(article: newsList![index]);
                           }),
                     )),
                   ],
@@ -84,84 +83,99 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
-Widget ArticleTile(Size size, BuildContext context, Article article) {
-  return GestureDetector(
-    onTap: () {
-      Navigator.of(context).push(MaterialPageRoute(
-          builder: (_) => DetailNews(
-                article: article,
-              )));
-    },
-    child: Container(
-      margin: EdgeInsets.all(12.0),
-      padding: EdgeInsets.only(bottom: 10, left: 5, right: 5),
-      decoration: BoxDecoration(
-          color: Colors.black,
-          borderRadius: BorderRadius.circular(12.0),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black12,
-              blurRadius: 3.0,
+class ArticleTiles extends StatefulWidget {
+  Article article;
+  ArticleTiles({required this.article});
+
+  @override
+  _ArticleTilesState createState() => _ArticleTilesState();
+}
+
+class _ArticleTilesState extends State<ArticleTiles> {
+  bool _isFavorite = false;
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(
+            builder: (_) => DetailNews(
+                  article: widget.article,
+                )));
+      },
+      child: Container(
+        margin: EdgeInsets.all(12.0),
+        padding: EdgeInsets.only(bottom: 10, left: 5, right: 5),
+        decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(12.0),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black12,
+                blurRadius: 3.0,
+              ),
+            ]),
+        child: Column(
+          children: [
+            Container(
+                height: size.height / 4,
+                width: size.width / 1.15,
+                alignment: Alignment.center,
+                child: widget.article.imageUrl != ""
+                    ? ClipRRect(
+                        borderRadius: BorderRadius.circular(13),
+                        child: Stack(
+                          children: [
+                            Image.network(
+                              widget.article.imageUrl,
+                              fit: BoxFit.cover,
+                            ),
+                            Align(
+                              alignment: Alignment.topRight,
+                              child: GestureDetector(
+                                  onTap: () {
+                                    setState(() {
+                                      _isFavorite = !_isFavorite;
+                                    });
+                                  },
+                                  child: Icon(Icons.favorite_border_outlined,
+                                      color: _isFavorite
+                                          ? Colors.yellow
+                                          : Colors.white)),
+                            )
+                          ],
+                        ),
+                      )
+                    : Text("Image can't be loaded")),
+            SizedBox(
+              height: 8.0,
             ),
-          ]),
-      child: Column(
-        children: [
-          Container(
-              height: size.height / 4,
-              width: size.width / 1.15,
-              alignment: Alignment.center,
-              child: article.imageUrl != ""
-                  ? ClipRRect(
-                      borderRadius: BorderRadius.circular(13),
-                      child: Stack(
-                        children: [
-                          Image.network(
-                            article.imageUrl,
-                            fit: BoxFit.cover,
-                          ),
-                          Align(
-                            alignment: Alignment.topRight,
-                            child: GestureDetector(
-                              onTap: (){
-                                       
-                      },
-                      
-                                child: Icon(
-                              Icons.favorite_border_outlined,
-                              color: Colors.white,
-                            )),
-                          )
-                        ],
-                      ),
-                    )
-                  : Text("Image can't be loaded")),
-          SizedBox(
-            height: 8.0,
-          ),
-          Container(
-            width: size.width / 1.1,
-            padding: EdgeInsets.symmetric(vertical: 5),
-            child: Text(
-              article.title,
-              style: TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.w500,
-                  color: Colors.white),
+            Container(
+              width: size.width / 1.1,
+              padding: EdgeInsets.symmetric(vertical: 5),
+              child: Text(
+                widget.article.title,
+                style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white),
+              ),
             ),
-          ),
-          SizedBox(
-            height: 8.0,
-          ),
-          Container(
-            width: size.width / 1.1,
-            padding: EdgeInsets.symmetric(vertical: 5),
-            child: Text(
-              article.description,
-              style: TextStyle(fontSize: 16, color: Colors.white),
+            SizedBox(
+              height: 8.0,
             ),
-          ),
-        ],
+            Container(
+              width: size.width / 1.1,
+              padding: EdgeInsets.symmetric(vertical: 5),
+              child: Text(
+                widget.article.description,
+                style: TextStyle(fontSize: 16, color: Colors.white),
+              ),
+            ),
+          ],
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
